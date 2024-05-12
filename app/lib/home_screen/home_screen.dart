6a1af1eaffe59fa3b2ac1ac292745e5d10039a12
Damace <boniphace10@gmail.com>
2024-05-12 10:51:15 +0300

    first commit2

--- app/lib/home_screen/home_screen.dart
+++ app/lib/home_screen/home_screen.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
-
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:getwidget/components/carousel/gf_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,15 +14,44 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreen extends State<HomeScreen> {
   final formkey = GlobalKey<FormState>();
 
+  final List<String> imageList = [
+    "https://cdn.pixabay.com/photo/2019/12/19/10/55/christmas-market-4705877_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2019/12/19/10/55/christmas-market-4705877_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2019/12/20/00/03/road-4707345_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2019/12/22/04/18/x-mas-4711785__340.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/22/07/09/spruce-1848543__340.jpg"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-          color: Colors.white,
-          width: double.infinity,
-          height: double.infinity,
-          child: Text("Center")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          GFCarousel(
+            autoPlay: true,
+            hasPagination: true,
+            viewportFraction: 1.0,
+            aspectRatio: 2,
+            items: imageList.map(
+              (url) {
+                return Container(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Image.network(url, fit: BoxFit.cover, width: 1000.0),
+                  ),
+                );
+              },
+            ).toList(),
+            onPageChanged: (index) {
+              setState(() {
+                index;
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
